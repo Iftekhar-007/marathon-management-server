5104d43a017d1bfb300de4ce45586efbe441ca29
Iftekhar-007 <shawonsp534@gmail.com>
2025-06-16 23:51:45 +0600

    marathons get

--- index.js
+++ index.js
@@ -2,10 +2,57 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@iftekharbases.ulu3uwc.mongodb.net/?retryWrites=true&w=majority&appName=IftekharBases`;
+
+// const uri =
+//   "mongodb+srv://<db_username>:<db_password>@iftekharbases.ulu3uwc.mongodb.net/?retryWrites=true&w=majority&appName=IftekharBases";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const database = client.db("marathonHUb");
+    const marathonsCollection = database.collection("marathons");
+
+    app.post("/marathons", async (req, res) => {
+      const marathon = req.body;
+
+      const result = await marathonsCollection.insertOne(marathon);
+      res.send(result);
+    });
+
+    app.get("/marathons", async (req, res) => {
+      const result = await marathonsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Marathon Hub Is Cooking By Me");
